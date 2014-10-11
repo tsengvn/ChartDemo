2a7bd0c4907565099df5479af9460633f69f43e8
Hien Ngo <hien.ngo@saritasa.com>
2014-10-11 10:55:01 +0700

    init commit

---
+++ src/com/tsengvn/chartdemo/ChartActivity.java
@@ -0,0 +1,93 @@
+package com.tsengvn.chartdemo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import com.shinobicontrols.charts.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+
+/**
+ * Copyright (c) 2014, Saritasa LLC (www.saritasa.com). All rights reserved.
+ *
+ * @author Hien Ngo
+ */
+public class ChartActivity extends Activity{
+    private ShinobiChart mChart;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shinobi);
+
+        if (savedInstanceState == null) {
+            bindViews();
+            bindData();
+        }
+    }
+
+    private void bindViews(){
+        ChartFragment chartFragment =
+                (ChartFragment) getFragmentManager().findFragmentById(R.id.chart);
+        mChart = chartFragment.getShinobiChart();
+        mChart.setTitle("Donated Money");
+        mChart.setLicenseKey(getString(R.string.shinobi_license));
+
+    }
+
+    private void bindData(){
+        //setup X-axis
+        DateTimeAxis xAxis = new DateTimeAxis();
+        xAxis.enableGesturePanning(true);
+        xAxis.enableGestureZooming(true);
+        xAxis.enableMomentumPanning(true);
+        xAxis.enableMomentumZooming(true);
+        mChart.addXAxis(xAxis);
+
+        //setup Y-axis
+        DateRange xDefaultRange = new DateRange(
+                new GregorianCalendar(2010, Calendar.JANUARY, 1).getTime(),
+                new GregorianCalendar(2011, Calendar.DECEMBER, 31).getTime());
+        xAxis.setDefaultRange(xDefaultRange);
+        NumberAxis yAxis = new NumberAxis();
+        mChart.addYAxis(yAxis);
+
+        //create sample data
+        SimpleDataAdapter<Date, Integer> dataAdapter = new SimpleDataAdapter<Date, Integer>();
+
+        GregorianCalendar date = new GregorianCalendar(2010, Calendar.JANUARY, 1);
+        Random random = new Random();
+        for (int i=0 ; i<60 ; i++){
+            dataAdapter.add(new DataPoint<Date, Integer>(date.getTime(), random.nextInt(100)));
+            date.add(GregorianCalendar.MONTH, 1);
+        }
+
+        LineSeries lineSeries = new LineSeries();
+        lineSeries.setDataAdapter(dataAdapter);
+        lineSeries.setEntryAnimation(SeriesAnimation.createGrowVerticalAnimation());
+        lineSeries.enableAnimation(true);
+        lineSeries.setSelectionMode(Series.SelectionMode.POINT_SINGLE);
+        lineSeries.setCrosshairEnabled(true);
+        styleLineChart(lineSeries);
+        mChart.addSeries(lineSeries);
+
+    }
+
+
+    private void styleLineChart(LineSeries aLineSeries){
+        int mainColor = getResources().getColor(R.color.line_color);
+        aLineSeries.getStyle().setLineColor(mainColor);
+        aLineSeries.getStyle().setLineWidth(1.5f);
+
+        PointStyle pointStyle = new PointStyle();
+        pointStyle.setPointsShown(true);
+        pointStyle.setColor(Color.GRAY);
+        pointStyle.setInnerColor(mainColor);
+        pointStyle.setRadius(4);
+        aLineSeries.getStyle().setPointStyle(pointStyle);
+        aLineSeries.getStyle().setSelectedPointStyle(pointStyle);
+    }
+}
